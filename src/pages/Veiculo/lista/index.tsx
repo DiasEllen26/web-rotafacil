f2ee6391ab3254b1b8ef30f0b7918a6f237cd5d4
DiasEllen26 <ellen.c.s.dias@outlook.com.br>
2023-11-28 18:18:19 -0300

    Implementando componenente lista nas paginas

--- src/pages/Veiculo/lista/index.tsx
+++ src/pages/Veiculo/lista/index.tsx
@@ -1,108 +1,90 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import { getAllByCollection } from "../../../utils/firebase/getAllByCollection";
 import { deleteById } from "../../../utils/firebase/deleteById";
-import { ButtonCadastrar, ButtonDeletar, ContainerButton, ContentContainer, ListContainer } from "./style";
-import { IRotas } from "../../../types/IRotas";
-import { Link } from "react-router-dom";
+import { ButtonCadastrar, ButtonDeletar, ContainerButton, ContentContainer, ListContainer } from "../../../components/Lista/style";
+import { IGestor } from "../../../types/IGestor";
 import { FaPencil, FaRegTrashCan } from "react-icons/fa6";
 import { Loading } from "../../../components/Loading/Loading";
 import Swal from "sweetalert2";
+import Lista from "../../../components/Lista";
 
-interface IVeiculoResponse {
-	id: string;
-	descricao: string;
-	assento: number;
-	placa: string;
-	id_rota: {
-		id: string
-	}
-}
+export default function Gestores() {
+  const navigate = useNavigate();
 
-export default function Veiculos(){
+  const [gestores, setGestores] = useState<IGestor[]>();
+  const [isLoading, setLoading] = useState<boolean>(true);
 
-	const navigate = useNavigate()
+  useEffect(() => {
+    getAllGestores();
+  }, []);
 
-	const [veiculos, setVeiculos] = useState<IVeiculoResponse[]>();
-	const [rotas, setRotas] = useState<IRotas[]>();
+  async function getAllGestores() {
+    try {
+      const data = await getAllByCollection("gestor") as unknown as IGestor[];
+      setGestores(data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  }
 
-	const [isLoading, setLoading] = useState<boolean>(true);
+  async function handleDelete(id: string) {
+    try {
+      setLoading(true);
+      await deleteById("gestor", id);
+      const newGestores = gestores?.filter((gestor) => gestor.id !== id);
+      setGestores(newGestores);
+      setLoading(false);
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Erro",
+        text: String(error),
+      });
+      setLoading(false);
+    }
+  }
 
-	async function getAllVeiculos() {
-		const [data, rotaData] = await Promise.all([
-			getAllByCollection("veiculo") as unknown as IVeiculoResponse[],
-			getAllByCollection('rota') as unknown as IRotas[]
-		])
-		console.log(data)
-		setVeiculos(data);
-		setRotas(rotaData)
-		setLoading(false)
-	}
+  const renderGestorFields = (gestor: IGestor) => [
+    <h1 key="nome">{gestor.nome}</h1>,
+    <h2 key="email">{gestor.email}</h2>,
+    <div key="editar">
+      <Link to={`/gestor/${gestor.id}/editar/`}>
+        <FaPencil />
+      </Link>
+    </div>,
+    <div key="deletar">
+      <ButtonDeletar onClick={() => handleDelete(gestor.id)}>
+        <FaRegTrashCan />
+      </ButtonDeletar>
+    </div>,
+  ];
 
-	useEffect(()=>{
-		getAllVeiculos();
-	},[])
+  return (
+    <>
+      <Loading visible={isLoading} />
+      <ContentContainer>
+        <ListContainer>
+          <Lista
+            items={gestores || []}
+            onDelete={handleDelete}
+            editPath="editar"
+            renderFields={renderGestorFields}
+          />
+        </ListContainer>
+      </ContentContainer>
 
-	async function handleDelete(id: string){
-		try{
-			setLoading(true)
-			await deleteById("veiculo", id);
-			const newVeiculos = veiculos?.filter(veiculo => veiculo.id !== id);
-			setVeiculos(newVeiculos);
-			setLoading(false)
-		}catch(error){
-			Swal.fire({
-				icon: "error",
-				title: "Erro",
-				text: String(error)
-			});
-		}
-	}
-
-
-	return(
-		<>
-		<Loading visible={isLoading} />
-		<ContentContainer>
-			<ListContainer>
-				{veiculos?.map(veiculo => (
-					<div key={veiculo.id}>
-						<h1>{veiculo.descricao}</h1>
-						{rotas
-							?.filter(rota => rota.id === veiculo.id_rota.id)
-							.map( rota => (
-								<h2 key={rota.id}>{rota.localPartida + " A " + rota.destino}</h2>
-							))
-						}
-						<h2>{"Placa: " + veiculo.placa}</h2>
-						<h2>{"Assentos: " + veiculo.assento}</h2>
-						<div>
-								<Link to={`${veiculo.id}/editar/`}>
-									<FaPencil />
-								</Link>
-							</div>
-							<div>
-								<ButtonDeletar
-									onClick={()=> handleDelete(veiculo.id)}
-								>
-									<FaRegTrashCan />
-								</ButtonDeletar>
-							</div>
-					</div>
-				))}
-			</ListContainer>
-		</ContentContainer>
-
-		<ContainerButton>
-			<ButtonCadastrar
-				onClick={() => {
-					navigate('/veiculo/cadastrar');
-				}}
-			>
-				Cadastrar
-			</ButtonCadastrar>
-		</ContainerButton>
-
-		</>
-	)
+      <ContainerButton>
+        <ButtonCadastrar
+          onClick={() => {
+            navigate("/gestor/cadastrar");
+          }}
+        >
+          Cadastrar
+        </ButtonCadastrar>
+      </ContainerButton>
+    </>
+  );
 }
